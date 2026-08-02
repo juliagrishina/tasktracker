4c8254ec8a319371cc5477770170f9107367a75f
juliagrishina <juliagrishina@users.noreply.github.com>
2026-08-03 02:18:56 +0300

    test: stabilize main worktree checks

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   preset: 'jest-expo',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testMatch: ['**/__tests__/**/*.test.ts?(x)'],
+  testPathIgnorePatterns: ['<rootDir>/.worktrees/'],
   moduleNameMapper: {
     '^\\.\\./data/data-source$': '<rootDir>/src/data/data-source.web.ts',
   },
