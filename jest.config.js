d368265a550b05d7dbfd6e58c4a20d5c233eac2c
juliagrishina1 <308745601+juliagrishina@users.noreply.github.com>
2026-08-31 13:35:50 +0300

    chore: upgrade Expo SDK to 57

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,5 @@ module.exports = {
   modulePathIgnorePatterns: ['<rootDir>/.worktrees/'],
   moduleNameMapper: {
     '^\\.\\./data/data-source$': '<rootDir>/src/data/data-source.web.ts',
-    '^expo-modules-core$': '<rootDir>/node_modules/expo/node_modules/expo-modules-core/src/index.ts',
-    '^expo-modules-core/(.*)$': '<rootDir>/node_modules/expo/node_modules/expo-modules-core/$1',
   },
 };
